4083b57c98eb4b0ce6fa75cf6ca8c9803fd2b108
Noa Horn <nhorn@pivotal.io>
2016-02-11 17:14:41 -0800

    set host python script

---
+++ ghost.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+import socket
+print socket.gethostbyaddr("nhorn-mbp") #replace the parameter with yours
+print socket.gethostbyaddr("nhorn-mbp.local") #replace the parameter
+print socket.gethostbyaddr("1.0.0.127.in-addr.arpa") #leave without change
