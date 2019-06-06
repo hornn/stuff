e509b3bded5f9c5a4971213bc99e15e02e99a4f9
Noa Horn <noa@unifisoftware.com>
2019-06-06 14:37:13 -0700

    stop unifi before build

--- shmita.py
+++ shmita.py
@@ -11,11 +11,12 @@ def shmita_year():
     year -= TEMPLE_DESTROYED
     # 1. add one year because the temple was destroyed in year 1 in the cycle
     year += 1
-    # 2. discard centuries, because each century is like 2 yovels (jubilees) - 50 years, which is 7 shmitot.
+    # 2. discard centuries, because each century is like 2 yovels (jubilees) - 50 years,
+    # which is 7 shmitot.
     centuries_num = year / 100
     year = year % 100
     # 3. add 2 years for each discarded century, because 7 shmitot is 49, not 50,
-    # so we need to account the missing years. (According to Rabbi Yehuda)
+    # so we need to add back the missing years. (According to Rabbi Yehuda)
     year += (centuries_num * 2)
     # 4. divide the single years (decades + singles) in 7 to get what year in the cycle we are in now
     shmita = year % 7
@@ -23,4 +24,3 @@ def shmita_year():
 
 
 shmita_year()
-
