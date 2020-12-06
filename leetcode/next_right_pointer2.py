6711ea97f2d1b3a2efd20e8fda4a6e9cc734794e
Noa Horn <noa@unifisoftware.com>
2020-12-06 15:59:31 -0800

    leetcode - populate next right pointers in each node 2, space o(1)

--- leetcode/next_right_pointer2.py
+++ leetcode/next_right_pointer2.py
@@ -11,7 +11,7 @@ class Node(object):
 """
 
 class Solution(object):
-    def connect(self, root):
+    def connect_with_stack(self, root):
         """
         :type root: Node
         :rtype: Node
@@ -37,4 +37,42 @@ class Solution(object):
                     new_queue.append(node.left)
             stack = new_queue
             
+        return root
+
+    def connect(self, root):
+        """
+        :type root: Node
+        :rtype: Node
+        """
+
+        if not root:
+            return root
+
+        # at every node, modify the next level's next field
+        # then use the next field of this node (that were updated before) to skip to the next note
+        # if there is no next field, skip to the next level (the first child of this level)
+
+        itr = root
+        next_level = None
+        prev = None
+        while itr:
+            # print(itr.val, next_level.val if next_level else None, prev.val if prev else None)
+            if itr.left:
+                if prev:
+                    prev.next = itr.left
+                else:
+                    next_level = itr.left
+                prev = itr.left
+            if itr.right:
+                if prev:
+                    prev.next = itr.right
+                else:
+                    next_level = itr.right
+                prev = itr.right
+            if itr.next:
+                itr = itr.next
+            else:
+                itr = next_level
+                next_level = None
+                prev = None
         return root
